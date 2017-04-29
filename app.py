cbd94b939618075aae245aadde6dfe82b7691f89
Teodor Garzdin <tsg@miracle.dk>
2017-04-29 11:32:08 +0200

    Add stops endpoints.

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from falcon import API
 from controllers.index import *
 from controllers.routes import *
+from controllers.stops import *
 
 app = API()
 app.add_route('/', Index())
 app.add_route('/routes', Routes())
 app.add_route('/routes/{id}', SingleRoute())
+app.add_route('/stops', Stops())
+app.add_route('/stops/{id}', SingleStop())
---
+++ controllers/stops.py
@@ -0,0 +1,27 @@
+from json import dumps
+from falcon import HTTPNotFound
+from data.utils import request
+from data.constants import stops_url
+
+__all__ = ['Stops', 'SingleStop']
+
+def get_stops():
+    return request(stops_url)
+
+class Stops:
+    def on_get(self, request, response):
+        """Stops route for the application"""
+        data = get_stops()
+        response.body = dumps(data)
+
+class SingleStop:
+    def on_get(self, request, response, id):
+        """Single stop route for the application"""
+        data = get_stops()
+        found = None
+        for stop in data:
+            if int(stop['id']) == int(id):
+                found = stop
+        if not found:
+            raise HTTPNotFound(description="Stop not found")
+        response.body = dumps(found)
