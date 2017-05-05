adb7be14a7893d4b7b5b31f4825d0146726bf289
Teodor Garzdin <teodorgarzedin@gmail.com>
2017-05-05 12:31:31 +0200

    Add a function to calculate the distance between two coordinates using the “Haversine distance” formula.

--- data/utils.py
+++ data/utils.py
@@ -1,4 +1,8 @@
-__all__ = ['seconds_to_minutes', 'request']
+from math import radians, cos, sin, asin, sqrt
+
+__all__ = ['seconds_to_minutes', 'request', 'haversine']
+
+AVG_EARTH_RADIUS = 6371 # km
 
 def seconds_to_minutes(seconds):
     from time import strftime, gmtime
@@ -31,3 +35,28 @@ def request(url):
         }
     else:
         return loads(response)
+
+
+def haversine(point1, point2):
+    """ Calculate the great-circle distance between two points on the Earth surface.
+    :input: two 2-tuples, containing the latitude and longitude of each point
+    in decimal degrees.
+    Example: haversine((45.7597, 4.8422), (48.8567, 2.3508))
+    :output: Returns the distance bewteen the two points.
+    The default unit is kilometers. Miles can be returned
+    if the ``miles`` parameter is set to True.
+    """
+    # unpack latitude/longitude
+    lat1, lng1 = point1
+    lat2, lng2 = point2
+
+    # convert all latitudes/longitudes from decimal degrees to radians
+    lat1, lng1, lat2, lng2 = map(radians, (lat1, lng1, lat2, lng2))
+
+    # calculate haversine
+    lat = lat2 - lat1
+    lng = lng2 - lng1
+    d = sin(lat * 0.5) ** 2 + cos(lat1) * cos(lat2) * sin(lng * 0.5) ** 2
+    h = 2 * AVG_EARTH_RADIUS * asin(sqrt(d))
+
+    return h  # in kilometers
